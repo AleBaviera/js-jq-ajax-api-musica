4cc66ed7b9b04b8b10a86f35ed882db55935d6ee
alebaviera <baviera.alessandra@gmailcom>
2019-07-31 07:57:55 +0200

    select genre

--- main.js
+++ main.js
@@ -11,44 +11,58 @@
 $( document ).ready(function() {
 
 
-    var dischi = 'https://flynn.boolean.careers/exercises/api/array/music';
+  var dischi = 'https://flynn.boolean.careers/exercises/api/array/music';
+
+  $.ajax({
+    url : dischi,
+    method : 'GET',
+    success : function(data){
+
+      $('select').change(function(){      //funzione cambio genere con la select
+
+        $('select option:selected').each(function(){
 
 
+        var scegli = $(this).text();
 
+        // $('.select button ').click(function(){
+        //   var scegli = $('.select input').val();
+         console.log(scegli);
 
+        var albums = data.response;
+        console.log(albums);
+        for (var i = 0; i < albums.length; i++) {
+          var source = $("#template").html();
+          var template = Handlebars.compile(source); //compila Handlebars
 
-      $.ajax({
-        url : dischi,
-        method : 'GET',
-        success : function(data){
+          var album = albums[i];
 
+          if (album.genre == scegli){ //confronto con il genere di ogni item
 
-          var albums = data.response;
-          console.log(albums);
-          for (var i = 0; i < albums.length; i++) {
-            var source = $("#template").html();
-            var template = Handlebars.compile(source);
+          var context = {
+          'poster': album.poster,
+          'title': album.title,
+          'author':album.author,
+          'genre':album.genre,
+          'year':album.year,
+          };
+          console.log(context);
 
-            var album = albums[i];
-
-            var context = {
-            'poster': album.poster,
-            'title': album.title,
-            'author':album.author,
-            'genre':album.genre,
-            'year':album.year,
-            };
-            console.log(context);
-            var insert = template(context);
-            $('.container.cds-container').append(insert);
+          var insert = template(context);
+          $('.cds-container').append(insert); //appende solo gli oggetti selezionati
+                                              // ma non rimuove i precedenti
 
           }
-        },
-        error :
-          $('.container .cds-container').append('errore')
+        }
+        });
+      });
+    },
+    error : function(richiesta, stato, errore){
+      $('.cds-container').append('errore',errore)
 
+    }
 
-      }
-    );
+  });
+
 
 });
